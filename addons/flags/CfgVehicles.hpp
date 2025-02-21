d43e3a7c1edf3858a28a0cb147074770afd37819
Chronophylos <nikolai@chronophylos.com>
2025-02-21 18:06:52 +0100

    Refactor flags addon interaction menu structure

--- addons/flags/CfgVehicles.hpp
+++ addons/flags/CfgVehicles.hpp
@@ -1,25 +1,28 @@
 #define MACRO_FLAG_ACTIONS \
     class ACE_Actions { \
         class ACE_MainActions { \
-            class GVAR(AddFlag) { \
-                displayName = CSTRING(AddFlag); \
-                condition = QUOTE(!(_this call FUNC(hasFlag))); \
-                statement = QUOTE(_this call FUNC(applyFlag)); \
-            }; \
-            class GVAR(ResetFlag) { \
-                displayName = CSTRING(ResetFlag); \
-                condition = QUOTE(_this call FUNC(hasFlag)); \
-                statement = QUOTE(_this call FUNC(resetFlag)); \
-            }; \
-            class GVAR(ApplyTexture) { \
-                displayName = CSTRING(ApplyTexture); \
-                condition = QUOTE(!(_this call FUNC(hasTexture))); \
-                statement = QUOTE(_this call FUNC(applyTexture)); \
-            }; \
-            class GVAR(ResetTexture) { \
-                displayName = CSTRING(ResetTexture); \
-                condition = QUOTE(_this call FUNC(hasTexture)); \
-                statement = QUOTE(_this call FUNC(resetTexture)); \
+            class GVAR(FlagMenu) { \
+                displayName = CSTRING(DisplayName); \
+                class GVAR(AddFlag) { \
+                    displayName = CSTRING(AddFlag); \
+                    condition = QUOTE(!(_this call FUNC(hasFlag))); \
+                    statement = QUOTE(_this call FUNC(applyFlag)); \
+                }; \
+                class GVAR(ResetFlag) { \
+                    displayName = CSTRING(ResetFlag); \
+                    condition = QUOTE(_this call FUNC(hasFlag)); \
+                    statement = QUOTE(_this call FUNC(resetFlag)); \
+                }; \
+                class GVAR(ApplyTexture) { \
+                    displayName = CSTRING(ApplyTexture); \
+                    condition = QUOTE(!(_this call FUNC(hasTexture))); \
+                    statement = QUOTE(_this call FUNC(applyTexture)); \
+                }; \
+                class GVAR(ResetTexture) { \
+                    displayName = CSTRING(ResetTexture); \
+                    condition = QUOTE(_this call FUNC(hasTexture)); \
+                    statement = QUOTE(_this call FUNC(resetTexture)); \
+                }; \
             }; \
         }; \
     };
